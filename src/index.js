2a6aaf46cbfa06be2c3911d21295d398948ec28f
grigorij1863 <grigorij1863@gmail.com>
2024-10-20 14:03:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
     </head>
     <body>
       <div class="container">
-        <h1>KastetTHC ${regionName}!</h1>
+        <h1>KastetTHC123 ${regionName}!</h1>
       </div>
     </body>
     </html>
