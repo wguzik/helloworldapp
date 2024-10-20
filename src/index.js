1d0632839aa7df43f1c7fa117cbfd5d2ef86d05b
Krzysztof <47270514+KriKristof@users.noreply.github.com>
2024-10-20 11:47:04 +0200

    Update index.js
    Line 29 edited.

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
     </head>
     <body>
       <div class="container">
-        <h1>Hello World from ${regionName}, yay!</h1>
+        <h1>Hello World from ${regionName}, yay! Nicely done!</h1>
       </div>
     </body>
     </html>
